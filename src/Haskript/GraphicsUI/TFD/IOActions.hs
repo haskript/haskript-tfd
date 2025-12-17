704c2c64c24aeb3045b952b17d101c0b978af6ec
liamzee <liam.zhu@protonmail.com>
2025-12-17 17:47:11 -0500

    Added tests, message box, beep, notify features. Still need to refactor to handle null cases.

--- src/Haskript/GraphicsUI/TFD/IOActions.hs
+++ src/Haskript/GraphicsUI/TFD/IOActions.hs
@@ -5,6 +5,10 @@ module Haskript.GraphicsUI.TFD.IOActions (
     beep,
     TFDNotifyIcon (..),
     notifyPopup,
+    TFDMBDialogType (..),
+    TFDMBIcon (..),
+    TFDMBButton (..),
+    unsanitizedMessageBox,
 ) where
 
 import Foreign.C.String (
@@ -12,37 +16,118 @@ import Foreign.C.String (
     withCString,
  )
 import Foreign.C.Types (
-
+    CInt (..),
  )
 
 -- CInt
 
-import Data.Coerce (coerce)
-
-foreign import capi "tinyfiledialogs.h tinyfd_beep" c_beep :: IO ()
+foreign import capi safe "tinyfiledialogs.h tinyfd_beep" c_beep :: IO ()
 
 -- | Triggers system beep.
 beep :: IO ()
 beep = c_beep
 
-foreign import capi "tinyfiledialogs.h tinyfd_notifyPopup"
-    c_notifyPopup :: CString -> CString -> CString -> IO Int
+foreign import capi safe "tinyfiledialogs.h tinyfd_notifyPopup"
+    c_notify_popup :: CString -> CString -> CString -> IO CInt
 
--- | Avoids text interface of notify icon.
+-- | Notify icon sum type constructors to avoid raw text.
 data TFDNotifyIcon
     = TFDNotifyInfo
     | TFDNotifyWarning
     | TFDNotifyError
+    deriving (Eq, Show, Enum, Bounded)
 
 -- | Triggers system notification. Does nothing on newer MacOS systems.
-notifyPopup :: String -> String -> TFDNotifyIcon -> IO Int
+notifyPopup ::
+    String
+    -- ^ Title
+    -> String
+    -- ^ Message
+    -> TFDNotifyIcon
+    -- ^ Notification Icon, TFDNotifyInfo, TFDNotifyError
+    -> IO Int
 notifyPopup title message iconType =
     withCString title \cTitle ->
         withCString message \cMessage ->
             withCString convertedIconType \cIconType ->
-                coerce <$> c_notifyPopup cTitle cMessage cIconType
+                fromIntegral <$> c_notify_popup cTitle cMessage cIconType
   where
     convertedIconType = case iconType of
         TFDNotifyInfo -> "info"
         TFDNotifyWarning -> "warning"
         TFDNotifyError -> "error"
+
+foreign import capi safe "tinyfiledialogs.h tinyfd_messageBox"
+    c_message_box ::
+        CString
+        -> CString
+        -> CString
+        -> CString
+        -> CInt
+        -> IO CInt
+
+-- | Message box dialogue type constructors to avoid raw text.
+data TFDMBDialogType
+    = TFDMBOk
+    | TFDMBOkCancel
+    | TFDMBYesNo
+    | TFDMBYesNoCancel
+    deriving (Eq, Show, Enum, Bounded)
+
+-- | Message box icon type constructors to avoid raw text.
+data TFDMBIcon
+    = TFDMBInfo
+    | TFDMBWarning
+    | TFDMBError
+    | TFDMBQuestion
+    deriving (Eq, Show, Enum, Bounded)
+
+-- | Message box button constructors to avoid raw text.
+data TFDMBButton
+    = TFDMBCancelNo
+    | TFDMBOkYes
+    | TFDMBNoInYesNoCancel
+    deriving (Eq, Show, Enum, Bounded)
+
+{- | Call to TFD's message box. Note that this does NOT sanitize and use of user inputs without
+sanitization is unsafe.
+-}
+unsanitizedMessageBox ::
+    String
+    -- ^ Title
+    -> String
+    -- ^ Message
+    -> TFDMBDialogType
+    -- ^ Dialog type, TFDMBOk, TFDMBOkCancel, TFDMBYesNo, TFDMBYesNoCancel
+    -> TFDMBIcon
+    -- ^ Icon type, TFDMBInfo, TFDMBWarning, TFDMBError, TFDMBQuestion
+    -> TFDMBButton
+    -- ^ Default return. Choose between TFDMBCancelNo, TFDMBOkYes, TFDBNoInYesNoCancel
+    -> IO (Int `Either` TFDMBButton)
+    -- ^ On failure, returns the error code.
+unsanitizedMessageBox title message dialogType iconType defaultButton =
+    withCString title \cTitle ->
+        withCString message \cMessage ->
+            withCString convertedDialogType \cDialogType ->
+                withCString convertedIconType \cIconType ->
+                    convertToEither <$> c_message_box cTitle cMessage cDialogType cIconType convertedDefaultButton
+  where
+    convertedDialogType = case dialogType of
+        TFDMBOk -> "ok"
+        TFDMBOkCancel -> "okcancel"
+        TFDMBYesNo -> "yesno"
+        TFDMBYesNoCancel -> "yesnocancel"
+    convertedIconType = case iconType of
+        TFDMBInfo -> "info"
+        TFDMBWarning -> "warning"
+        TFDMBError -> "error"
+        TFDMBQuestion -> "question"
+    convertedDefaultButton = case defaultButton of
+        TFDMBCancelNo -> 0
+        TFDMBOkYes -> 1
+        TFDMBNoInYesNoCancel -> 2
+    convertToEither = \case
+        0 -> Right TFDMBCancelNo
+        1 -> Right TFDMBOkYes
+        2 -> Right TFDMBNoInYesNoCancel
+        n -> Left $ fromIntegral n
--- test/Main.hs
+++ test/Main.hs
@@ -3,24 +3,50 @@ module Main (main) where
 import Data.Foldable (traverse_)
 
 import Haskript.GraphicsUI.TFD.IOActions (
+    TFDMBButton (..),
+    TFDMBDialogType (..),
+    TFDMBIcon (..),
     TFDNotifyIcon (..),
     beep,
     notifyPopup,
+    unsanitizedMessageBox,
  )
 
 main :: IO ()
 main = do
     beepTest -- beep function
     notifyTest -- Notify tests of icons.
+    messageBoxTest -- Message boxes.
 
 beepTest :: IO ()
 beepTest = beep
 
+-- | Note, need to install null value support since CString will avoid null pointers.
 notifyTest :: IO ()
-notifyTest =
+notifyTest = traverse_ go ["", "Success!"] -- need to examine null cases.
+  where
+    go text =
+        traverse_
+            (uncurry $ flip notifyPopup text)
+            [ ("Info", TFDNotifyInfo)
+            , ("Warning", TFDNotifyWarning)
+            , ("Error", TFDNotifyError)
+            ]
+
+messageBoxTest :: IO () -- need to refactor to examine null string cases.
+messageBoxTest =
     traverse_
-        (uncurry $ flip notifyPopup "Success!")
-        [ ("Info", TFDNotifyInfo)
-        , ("Warning", TFDNotifyWarning)
-        , ("Error", TFDNotifyError)
-        ]
+        (((print =<<) .) $ uncurry $ uncurry $ unsanitizedMessageBox "Test" "Success!")
+        messageBoxTests
+
+-- | Container to generate all basic valid inputs for messageBoxTest.
+messageBoxTests :: [((TFDMBDialogType, TFDMBIcon), TFDMBButton)]
+messageBoxTests = do
+    diaType <- [minBound .. maxBound]
+    iconType <- [minBound .. maxBound]
+    defButton <- case diaType of
+        TFDMBOk -> [TFDMBOkYes]
+        TFDMBOkCancel -> [TFDMBCancelNo, TFDMBOkYes]
+        TFDMBYesNo -> [TFDMBCancelNo, TFDMBOkYes]
+        TFDMBYesNoCancel -> [TFDMBCancelNo, TFDMBOkYes, TFDMBNoInYesNoCancel]
+    [((diaType, iconType), defButton)]
