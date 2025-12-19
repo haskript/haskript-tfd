66e4087a7133e1ef72cd6469982b29d14d719374
liamzee <liam.zhu@protonmail.com>
2025-12-19 03:10:03 -0500

    Fix issue with compilation caused by incorrectly set up references.

--- src/Haskript/GraphicsUI/TFD/Internal/FFI.hs
+++ src/Haskript/GraphicsUI/TFD/Internal/FFI.hs
@@ -24,19 +24,19 @@ import Foreign.C.Types (CInt (..), CUChar)
 import Foreign.Ptr (Ptr)
 
 -- Main program functions
-foreign import capi unsafe "tinyfiledialogs.h tinyfd_version"
+foreign import capi unsafe "tinyfiledialogs.h &tinyfd_version"
     c_tinyfd_version :: Ptr CString
-foreign import capi unsafe "tinyfiledialogs.h tinyfd_needs"
+foreign import capi unsafe "tinyfiledialogs.h &tinyfd_needs"
     c_tinyfd_requirements :: Ptr CString
-foreign import capi unsafe "tinyfiledialogs.h tinyfd_verbose"
+foreign import capi unsafe "tinyfiledialogs.h &tinyfd_verbose"
     c_tinyfd_verbose :: Ptr CInt
-foreign import capi unsafe "tinyfiledialogs.h tinyfd_silent"
+foreign import capi unsafe "tinyfiledialogs.h &tinyfd_silent"
     c_tinyfd_silent :: Ptr CInt
-foreign import capi unsafe "tinyfiledialogs.h tinyfd_allowCursesDialogs"
+foreign import capi unsafe "tinyfiledialogs.h &tinyfd_allowCursesDialogs"
     c_tinyfd_allow_curses_dialogs :: Ptr CInt
-foreign import capi unsafe "tinyfiledialogs.h tinyfd_forceConsole"
+foreign import capi unsafe "tinyfiledialogs.h &tinyfd_forceConsole"
     c_tinyfd_force_console :: Ptr CInt
-foreign import capi unsafe "tinyfiledialogs.h tinyfd_response"
+foreign import capi unsafe "tinyfiledialogs.h &tinyfd_response"
     c_tinyfd_response_buffer :: Ptr CString
 
 -- Main program functions.
